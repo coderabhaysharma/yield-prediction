79bedbc0732c3bdccda2f0f9fcf13a97c6f48a3c
Abhay Sharma <115949147+coderabhaysharma@users.noreply.github.com>
2025-03-30 22:47:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = joblib.load("model.pkl")
 
 # Initialize Flask app
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173"])
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
 
 @app.route('/')
